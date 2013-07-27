b23d910b4eb18b1fa3e57a8386d4b35651872816
achaoac <362256632@qq.com>
2013-07-27 15:24:19 +0800

    update

---
+++ zymobcms-ios/ZYMobCMS/ZYMobCMS/ZYMobCMSceshi.h
@@ -0,0 +1,13 @@
+//
+//  ZYMobCMSceshi.h
+//  ZYMobCMS
+//
+//  Created by cnyzn on 13-7-27.
+//  Copyright (c) 2013年 ZYVincent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZYMobCMSceshi : NSObject
+
+@end
