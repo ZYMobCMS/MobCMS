cdb323c7ed634c208e723ba5d0a3838d7f3a65b4
achaoac <362256632@qq.com>
2013-07-27 13:31:36 +0800

    add ztmenuViewController

---
+++ zymobcms-ios/ZYMobCMS/ZYMobCMS/ZTMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZTMenuViewController.h
+//  ZYMobCMS
+//
+//  Created by cnyzn on 13-7-27.
+//  Copyright (c) 2013年 ZYVincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZTMenuViewController : UITabBarController
+
+@end
---
+++ zymobcms-ios/ZYMobCMS/ZYMobCMS/ZTMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZTMenuViewController.m
+//  ZYMobCMS
+//
+//  Created by cnyzn on 13-7-27.
+//  Copyright (c) 2013年 ZYVincent. All rights reserved.
+//
+
+#import "ZTMenuViewController.h"
+
+@interface ZTMenuViewController ()
+
+@end
+
+@implementation ZTMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
