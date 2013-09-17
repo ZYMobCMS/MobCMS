dd9ee775dda5b92532e7455ca929173d44f49400
zyprosoft <hutao5262705@hotmail.com>
2013-09-17 23:58:27 +0800

    update

--- zymobcms-server/zymobcms/protected/modules/Admin/views/article/index.php
+++ zymobcms-server/zymobcms/protected/modules/Admin/views/article/index.php
@@ -12,6 +12,133 @@ $this->menu=array(
 );
 ?>
 
+<?php $this->widget('bootstrap.widgets.TbButton', array(
+    'buttonType'=>'button',
+    'type'=>'primary',
+    'label'=>'Toggle me',
+    'toggle'=>true,
+)); ?>
+
+<?php $this->widget('bootstrap.widgets.TbButtonGroup', array(
+    'type' => 'primary',
+    'toggle' => 'checkbox', // 'checkbox' or 'radio'
+    'buttons' => array(
+        array('label'=>'Left','url'=>'index.php'),
+        array('label'=>'Middle'),
+        array('label'=>'Right'),
+    ),
+)); ?>
+
+<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+    'links'=>array('高级'=>'#', '无敌'),
+)); ?>
+
+
+<?php $this->widget('bootstrap.widgets.TbMenu', array(
+    'type'=>'pills', // '', 'tabs', 'pills' (or 'list')
+    'stacked'=>false, // whether this is a stacked menu
+    'items'=>array(
+        array('label'=>'Home', 'url'=>'#', 'active'=>true),
+        array('label'=>'Profile', 'url'=>'#',),
+        array('label'=>'Messages', 'url'=>'#',),
+    ),
+)); ?>
+
+<?php $this->widget('bootstrap.widgets.TbMenu', array(
+    'type'=>'list',
+    'items'=>array(
+        array('label'=>'LIST HEADER'),
+        array('label'=>'Home', 'icon'=>'home', 'url'=>'#', 'active'=>true),
+        array('label'=>'Library', 'icon'=>'book', 'url'=>'#'),
+        array('label'=>'Application', 'icon'=>'pencil', 'url'=>'#'),
+        array('label'=>'ANOTHER LIST HEADER'),
+        array('label'=>'Profile', 'icon'=>'user', 'url'=>'#'),
+        array('label'=>'Settings', 'icon'=>'cog', 'url'=>'#'),
+        array('label'=>'Help', 'icon'=>'flag', 'url'=>'#'),
+    ),
+)); ?>
+
+	<?php $this->widget('bootstrap.widgets.TbAlert', array(
+        'block'=>true, // display a larger alert block?
+        'fade'=>true, // use transitions?
+        'closeText'=>'&times;', // close link text - if set to false, no close link is displayed
+        'alerts'=>array( // configurations per alert type
+            'success'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
+        ),
+    )); ?>
+    
+    <?php $this->widget('bootstrap.widgets.TbLabel', array(
+    'type'=>'success', // 'success', 'warning', 'important', 'info' or 'inverse'
+    'label'=>'Success',
+	)); ?>
+	
+	<?php $this->widget('bootstrap.widgets.TbBadge', array(
+    'type'=>'success', // 'success', 'warning', 'important', 'info' or 'inverse'
+    'label'=>'2',
+	)); ?>
+	
+	<?php $this->widget('bootstrap.widgets.TbCarousel', array(
+    'items'=>array(
+        array('image'=>'http://placehold.it/770x400&text=First+thumbnail', 'label'=>'First Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
+        array('image'=>'http://placehold.it/770x400&text=Second+thumbnail', 'label'=>'Second Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
+        array('image'=>'http://placehold.it/770x400&text=Third+thumbnail', 'label'=>'Third Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
+    ),
+	)); ?>
+	
+	<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'myModal')); ?>
+ 
+<div class="modal-header">
+    <a class="close" data-dismiss="modal">&times;</a>
+    <h4>Modal header</h4>
+</div>
+ 
+<div class="modal-body">
+    <p>One fine body...</p>
+</div>
+ 
+<div class="modal-footer">
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'type'=>'primary',
+        'label'=>'Save changes',
+        'url'=>'#',
+        'htmlOptions'=>array('data-dismiss'=>'modal'),
+    )); ?>
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'label'=>'Close',
+        'url'=>'#',
+        'htmlOptions'=>array('data-dismiss'=>'modal'),
+    )); ?>
+</div>
+ 
+<?php $this->endWidget(); ?>
+
+<?php $this->widget('bootstrap.widgets.TbButton', array(
+    'label'=>'Click me',
+    'type'=>'primary',
+    'htmlOptions'=>array(
+        'data-toggle'=>'modal',
+        'data-target'=>'#myModal',
+    ),
+)); ?>
+
+<?php $this->widget('bootstrap.widgets.TbButton', array(
+    'label'=>'Click me',
+    'type'=>'danger',
+    'htmlOptions'=>array('data-title'=>'A Title', 'data-content'=>'And here\'s some amazing content. It\'s very engaging. right?', 'rel'=>'popover'),
+)); ?>
+
+<?php $this->widget('bootstrap.widgets.TbTabs', array(
+    'type'=>'pills', // 'tabs' or 'pills'
+    'tabs'=>array(
+        array('label'=>'Home', 'content'=>'Raw denim you probably haven\'t heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.', 'active'=>true),
+        array('label'=>'Profile', 'content'=>'Food truck fixie locavore, accusamus mcsweeney\'s marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.'),
+        array('label'=>'Dropdown', 'items'=>array(
+            array('label'=>'@fat', 'content'=>'Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney\'s organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven\'t heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.'),
+            array('label'=>'@mdo', 'content'=>'Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral, mustache readymade thundercats keffiyeh craft beer marfa ethical. Wolf salvia freegan, sartorial keffiyeh echo park vegan.'),
+        )),
+    ),
+)); ?>
+    
 <h1>文章列表</h1>
 
 <?php $this->widget('bootstrap.widgets.TbGridView', array(
--- zymobcms-server/zymobcms/protected/modules/Admin/views/article/view.php
+++ zymobcms-server/zymobcms/protected/modules/Admin/views/article/view.php
@@ -18,26 +18,28 @@ $this->menu=array(
 
 <h1>View Article #<?php echo $model->id; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
+<?php $this->widget('bootstrap.widgets.TbDetailView', array(
+	'type'=>array('striped','bordered'),
+	'data'=>array('id'=>$model->id,'title'=>$model->title,'source'=>$model->source,'summary'=>$model->summary,
+	'author'=>$model->author,'content'=>$model->content,'publish_time'=>$model->publish_time,'create_time'=>$model->create_time,
+	'images'=>$model->images,'links'=>$model->links,'comment_count'=>$model->comment_count,'favorite_count'=>$model->favorite_count,
+	'commentable'=>$model->commentable,'hot_news'=>$model->hot_news,'status'=>$model->status
+	),
 	'attributes'=>array(
-		'id',
-		'title',
-		'source',
-		'summary',
-		'author',
-		'content',
-		'publish_time',
-		'create_time',
-		'status',
-		'images',
-		'update_time',
-		'tab_type_id',
-		'category_id',
-		'links',
-		'comment_count',
-		'favorite_count',
-		'commentable',
-		'hot_news',
+		array('name'=>'id','label'=>'编号'), 
+		array('name'=>'title','label'=>'标题'),
+		array('name'=>'source','label'=>'来源'),
+		array('name'=>'summary','label'=>'概要'),
+		array('name'=>'author','label'=>'作者'),
+		array('name'=>'content','label'=>'内容'),
+		array('name'=>'publish_time','label'=>'时间'),
+		array('name'=>'create_time','label'=>'添加时间'),
+		array('name'=>'status','label'=>'状态'),
+		array('name'=>'images','label'=>'图片'),
+		array('name'=>'links','label'=>'原文链接'),
+		array('name'=>'comment_count','label'=>'评论数'),
+		array('name'=>'favorite_count','label'=>'收藏数'),
+		array('name'=>'commentable','label'=>'可否评论'),
+		array('name'=>'hot_news','label'=>'热门新闻'),
 	),
 )); ?>
